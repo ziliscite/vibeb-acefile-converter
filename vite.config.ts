bc111e604b3b7e0a7d5aa9f9ba180f32efee6cd9
Zil <Manzil.akbar@gmail.com>
2025-07-29 18:47:49 +0700

    deploy?

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/vibeb-acefile-converter",
   server: {
     proxy: {
       '/api': {
@@ -20,7 +21,6 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  base: process.env.NODE_ENV === 'production' ? '/acefile-converter/' : '/',
   build: {
     outDir: 'dist',
     sourcemap: true,
